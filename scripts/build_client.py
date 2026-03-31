b15bbb1fbf9331c94805937942c91d3624cf371c
bbusn <benoit.busnardo@gmail.com>
2026-03-31 23:10:44 +0200

    fix: Fix formatting of version file generation in build_client.py for windows

--- scripts/build_client.py
+++ scripts/build_client.py
@@ -63,34 +63,36 @@ if PLATFORM == "windows":
 
     with open(version_file, "w", encoding="utf-8") as f:
         f.write(f"""VSVersionInfo(
-    ffi=FixedFileInfo(
-        filevers={version_tuple},
-        prodvers={version_tuple},
-        mask=0x3f,
-        flags=0x0,
-        OS=0x40004,
-        fileType=0x1,
-        subtype=0x0,
-        date=(0, 0)
-    ),
-    kids=[
-        StringFileInfo(
-        [
-        StringTable(
-            u'040904B0',
-            [StringStruct(u'CompanyName', u'{COMPANY}'),
-            StringStruct(u'FileDescription', u'{DISPLAY_NAME}'),
-            StringStruct(u'FileVersion', u'{VERSION}'),
-            StringStruct(u'InternalName', u'sysmon'),
-            StringStruct(u'LegalCopyright', u'{COPYRIGHT}'),
-            StringStruct(u'OriginalFilename', u'sysmon.exe'),
-            StringStruct(u'ProductName', u'{DISPLAY_NAME}'),
-            StringStruct(u'ProductVersion', u'{VERSION}')])
-        ]),
-        VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
-    ]
-    )
-    """)
+            ffi=FixedFileInfo(
+                filevers={version_tuple},
+                prodvers={version_tuple},
+                mask=0x3f,
+                flags=0x0,
+                OS=0x40004,
+                fileType=0x1,
+                subtype=0x0,
+                date=(0, 0)
+            ),
+            kids=[
+                StringFileInfo([
+                StringTable(
+                    u'040904B0',
+                    [
+                    StringStruct(u'CompanyName', u'{COMPANY}'),
+                    StringStruct(u'FileDescription', u'{DISPLAY_NAME}'),
+                    StringStruct(u'FileVersion', u'{VERSION}'),
+                    StringStruct(u'InternalName', u'sysmon'),
+                    StringStruct(u'LegalCopyright', u'{COPYRIGHT}'),
+                    StringStruct(u'OriginalFilename', u'sysmon.exe'),
+                    StringStruct(u'ProductName', u'{DISPLAY_NAME}'),
+                    StringStruct(u'ProductVersion', u'{VERSION}')
+                    ]
+                )
+                ]),
+                VarFileInfo([VarStruct(u'Translation', [1033, 1200])])
+            ]
+            )
+            """)
 
 # -----------------------------
 # Build with PyInstaller
